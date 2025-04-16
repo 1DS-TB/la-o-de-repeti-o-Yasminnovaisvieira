ce1d3cf1714ac4f09c029d1b32dd58f492727498
Yasmin <167587232+Yasminnovaisvieira@users.noreply.github.com>
2025-04-16 16:12:19 -0300

    Atualizando - 16/04/2025

--- exercicio3.py
+++ exercicio3.py
@@ -1,6 +1,6 @@
 # Peça ao usuário um número e exiba sua tabuada de multiplicação de 1 a 10 usando um laço for.
 
-numero = int(input("Digite um número para visualizar a sua tabuada de 1 a 10: "))
+numero = int(input("Digite um número para ver sua tabuada de 1 a 10: "))
 
 for i in range(1, 11):
     print(f"{numero} x {i} = {numero * i}")
