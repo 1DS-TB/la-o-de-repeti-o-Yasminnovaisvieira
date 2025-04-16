7dc625956837820de87b398843b703fe342a3efc
Yasmin <167587232+Yasminnovaisvieira@users.noreply.github.com>
2025-04-16 16:34:27 -0300

    Atualizando - 16/04/2025

--- exercicio8.py
+++ exercicio8.py
@@ -1,14 +1,20 @@
 # Calcule a soma da série harmônica até N termos: S = 1 + 1/2 + 1/3 + ... + 1/N. Arredonde o resultado para 2 casas decimais.
 
-N = int(input("Digite um número: "))
+divisor = int(input("Insira o número: "))
 
-resultado = 0
+n = 1
+serie_harmonica = []
+soma = 0
 
-if N > 0:
-    for i in range(1, N + 1):
-        resultado += 1 / i
-        print(f"{1 / i}", end=" + ")
-
-    print(f"\nO resultado final da soma é: {resultado:.2f}")
-elif N <= 0:
+if divisor <= 0:
     print("INVALIDO")
+    
+else:
+    while n <= divisor:
+        serie_harmonica.append(f"1/{n}")
+        serie = 1/n
+        n +=1
+        soma += serie
+        
+    print(serie_harmonica)
+    print(f"A soma da série harmônica é igual a: {soma:.2f}")
