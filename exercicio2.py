ace10141b692cccf80c22e35b2f853b8b4673978
Yasmin <167587232+Yasminnovaisvieira@users.noreply.github.com>
2025-04-16 16:32:47 -0300

    Atualizando - 16/04/2025

--- exercicio2.py
+++ exercicio2.py
@@ -1,16 +1,15 @@
 # Peça ao usuário um número inteiro positivo N e calcule a soma de todos os números de 1 até N usando um laço while.
 
-N = int(input("Digite um número positivo: "))
+numero = int(input("Por favor, insira um número inteiro positivo: "))
+soma = 0
 
-if N > 0:
-    numero = 0
-    soma = 0
-
-    while numero <= N:
-        soma += numero
-        numero += 1
-
-    print(f"O resultado da soma de 1 até {N} é: {soma}")
-
-elif N <= 0:
+if numero <= 0:
     print("INVALIDO")
+    
+else:
+    numero += 1
+    while numero > 1:
+        numero -= 1
+        soma+= numero
+
+print(f"O resultado é: {soma}")
